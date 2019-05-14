479f5d5acb5dace219933f9098d0dabaf075a70f
yushixin <wysx95@163.com>
2019-05-14 14:28:24 +0800

    333

--- miniprogram/pages/comment/comment.js
+++ miniprogram/pages/comment/comment.js
@@ -7,11 +7,63 @@ Page({
   data: {
     detail: {},
     content: '', // 评价的内容
-    score: this.data.score,
-    movieid: this.data.movieId,
-    fileIds: this.data.fileIds
-
+    score: 5, // 评价的分数
+    images: [], // 上传的图片
+    fileIds: [],
+    movieId: -1
   }, 
+  submit: function () {
+    wx.showLoading({
+      title: '评论中',
+    })
+    console.log(this.data.content, this.data.score);
+
+    // 上传图片到云存储
+    let promiseArr = [];
+    for (let i = 0; i < this.data.images.length; i++) {
+      promiseArr.push(new Promise((reslove, reject) => {
+        let item = this.data.images[i];
+        let suffix = /\.\w+$/.exec(item)[0]; // 正则表达式，返回文件扩展名
+        wx.cloud.uploadFile({
+          cloudPath: new Date().getTime() + suffix, // 上传至云端的路径
+          filePath: item, // 小程序临时文件路径
+          success: res => {
+            // 返回文件 ID
+            console.log(res.fileID)
+            this.setData({
+              fileIds: this.data.fileIds.concat(res.fileID)
+            });
+            reslove();
+          },
+          fail: console.error
+        })
+      }));
+    }
+
+    Promise.all(promiseArr).then(res => {
+      // 插入数据
+      db.collection('comment').add({
+        data: {
+          content: this.data.content,
+          score: this.data.score,
+          movieid: this.data.movieId,
+          fileIds: this.data.fileIds
+        }
+      }).then(res => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '评价成功',
+        })
+      }).catch(err => {
+        wx.hideLoading();
+        wx.showToast({
+          title: '评价失败',
+        })
+      })
+
+    });
+
+  },
   onContentChange: function (event) {
     this.setData({
       content: event.detail
@@ -21,6 +73,22 @@ Page({
     this.setData({
       score: event.detail
     });
+  },
+  uploadImg: function () {
+    // 选择图片
+    wx.chooseImage({
+      count: 9,
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: res => {
+        // tempFilePath可以作为img标签的src属性显示图片
+        const tempFilePaths = res.tempFilePaths
+        console.log(tempFilePaths);
+        this.setData({
+          images: this.data.images.concat(tempFilePaths)
+        });
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
